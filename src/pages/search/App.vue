a8a2e9dab00ba51db16189525cf206b7c2c8182a
Dusty <mrgeek484@gmail.com>
2021-08-01 11:08:55 +0800

    commit

--- src/pages/search/App.vue
+++ src/pages/search/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="markdown-body">
     <h1>Search NPM</h1>
-    <form class="search-container">
-      <input v-model="search" type="text" id="search-bar" placeholder="What can I help you with today?">
-      <a href="#" @click="getPkg()"><img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png"></a>
-    </form>
-    <div class="results">
+    <div class="ui icon input fluid">
+      <input v-model="search" type="text" placeholder="Search...">
+      <i @click="getPkg()" class="inverted circular search link icon"></i>
+    </div>
+    <div class="results ui container">
       <p>NPM Search results for: {{ search }}</p>
-      <table full>
+      <table class="ui celled striped table">
         <thead full>
   <tr>
+    <th>No.</th>
     <th>Package Name</th>
     <th>Description</th>
     <th>Version</th>
@@ -17,6 +18,7 @@
 </thead>
 <tbody full>
   <tr  v-for="(pkg, index) in pkgs" :key="index" full>
+    <td>{{ index + 1 }}</td>
     <td> <a :href="pkg.package.links.npm" target="_blank">{{ pkg.package.name }}</a> </td>
     <td>{{ pkg.package.description }}</td>
     <td>{{ pkg.package.version }}</td>
@@ -66,50 +68,12 @@ export default {
 </script>
 
 <style scoped>
-@import '../../../def.css';
-.search-container {
-  width: 95%;
-  display: block;
-  margin: 10px auto;
-  border: 1px solid #D0CFCE;
-}
-
-input#search-bar {
-  margin: 0 auto;
+@import 'https://cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css';
+input[v-model=search] {
+  margin-bottom: 20px;
   width: 100%;
-  height: 45px;
-  padding: 0 20px;
-  font-size: 1rem;
-  outline: none;
-  left: -30px;
 }
-input#search-bar:focus {
-  border: 1px solid #008ABF;
-  transition: 0.35s ease;
-  color: #008ABF;
-}
-input#search-bar:focus::-webkit-input-placeholder {
-  transition: opacity 0.45s ease;
-  opacity: 0;
-}
-input#search-bar:focus::-moz-placeholder {
-  transition: opacity 0.45s ease;
-  opacity: 0;
-}
-input#search-bar:focus:-ms-placeholder {
-  transition: opacity 0.45s ease;
-  opacity: 0;
-}
-
-.search-icon {
-  position: relative;
-  float: right;
-  width: 75px;
-  height: 75px;
-  top: -62px;
-  right: -76px;
-}
-table {
-  overflow: auto;
+.results {
+  margin: 20px;
 }
 </style>
