9c894fe0c4e292f51c6d5ce33e88ada8a2faea9d
Dusty <mrgeek484@gmail.com>
2021-08-05 17:52:03 +0800

    commit

--- commit.bat
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -am commit
-git push origin master
-git push deploy --all
--- deploy.bat
+++
@@ -1,2 +0,0 @@
-cd dist
-surge teardown det171.surge.sh && surge --domain det171.surge.sh --project . && cd ..
